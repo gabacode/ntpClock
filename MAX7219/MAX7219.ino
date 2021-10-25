71359c0b9c11536d854f73e1edd0befce0859498
gabacode <gaba41@gmail.com>
2021-10-25 15:54:36 +0200

    added matrix display

---
+++ MAX7219/MAX7219.ino
@@ -0,0 +1,42 @@
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 4
+
+#define DATA_PIN 15
+#define CS_PIN 13
+#define CLK_PIN 12
+
+const char *ssid     = "your-wifi-name";
+const char *password = "your-wifi-password";
+const long utcOffsetInSeconds = 7200;
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+void setup() {
+  P.begin();
+  P.setIntensity(1);
+  P.setTextAlignment(PA_CENTER);
+  P.setCharSpacing(2);
+  WiFi.begin(ssid, password);
+  P.print("88:88");
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+  timeClient.begin();
+}
+
+void loop() {
+  timeClient.update();
+  P.print(String(timeClient.getHours()) + ":" + String(timeClient.getMinutes()));
+  delay(1000);
+}
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 ## Arduino 7-segments clock that keeps sync with NTP server
-![nodemcu-esp8266](https://github.com/gabacode/ntpClock/blob/master/images/prev.jpg?raw=true)
+![nodemcu-esp8266](https://github.com/gabacode/ntpClock/blob/master/images/matrix.jpg?raw=true)
 
-## Required libraries
+## 7-segments display Requirements
 
 - [ESP8266WiFi](https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WiFi)  
 - [NTPClient](https://github.com/arduino-libraries/NTPClient)  
 - [TM1637](https://github.com/avishorp/TM1637)
+
+## Matrix Display
+
+- [ESP8266WiFi](https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WiFi)  
+- [NTPClient](https://github.com/arduino-libraries/NTPClient)
+- [MD_Parola](https://www.arduino.cc/reference/en/libraries/md_parola/)
